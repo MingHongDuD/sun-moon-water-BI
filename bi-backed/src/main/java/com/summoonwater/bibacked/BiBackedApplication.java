b7ddee47433754d0219fd9adb4e50c6b119affd0
木土吹冰灯 <ming.hong.du.d@gmail.com>
2024-08-22 11:36:40 +0800

    项目初始化

---
+++ bi-backed/src/main/java/com/summoonwater/bibacked/BiBackedApplication.java
@@ -0,0 +1,13 @@
+package com.summoonwater.bibacked;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiBackedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BiBackedApplication.class, args);
+    }
+
+}
---
+++ bi-backed/src/test/java/com/summoonwater/bibacked/BiBackedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.summoonwater.bibacked;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiBackedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
