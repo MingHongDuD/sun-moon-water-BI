efe9f0bdac77eb16d9da536eed0c54b2aae395e0
SummoonWater <ming.hong.du.d@gmail.com>
2024-08-22 15:04:30 +0800

    删除多余引入的依赖

--- bi-backed/src/main/java/com/summoonwater/bibacked/exception/GlobalExceptionHandler.java
+++ bi-backed/src/main/java/com/summoonwater/bibacked/exception/GlobalExceptionHandler.java
@@ -17,13 +17,13 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(BusinessException.class)
-    public BaseResponse<?> businessExceptionHandler(BusinessException e) {
+    public <T> BaseResponse<T> businessExceptionHandler(BusinessException e) {
         log.error("BusinessException", e);
         return ResultUtils.error(e.getCode(), e.getMessage());
     }
 
     @ExceptionHandler(RuntimeException.class)
-    public BaseResponse<?> runtimeExceptionHandler(RuntimeException e) {
+    public <T> BaseResponse<T> runtimeExceptionHandler(RuntimeException e) {
         log.error("RuntimeException", e);
         return ResultUtils.error(ErrorCode.SYSTEM_ERROR, "系统错误");
     }
