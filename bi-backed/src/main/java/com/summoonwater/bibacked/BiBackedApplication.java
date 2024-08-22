80e2b23af43792001d0c30481674c28ae40d5696
SummoonWater <ming.hong.du.d@gmail.com>
2024-08-22 12:11:50 +0800

    启动类配置完毕

--- bi-backed/src/main/java/com/summoonwater/bibacked/BiBackedApplication.java
+++ bi-backed/src/main/java/com/summoonwater/bibacked/BiBackedApplication.java
@@ -1,9 +1,20 @@
 package com.summoonwater.bibacked;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
+/**
+ * @author SummoonWater
+ * @date 2024/8/22 12:07
+ * @description 入口类
+ */
 @SpringBootApplication
+@MapperScan()
+//@EnableCaching("com.summoonwater.bibacked.mapper")
+@EnableAspectJAutoProxy(proxyTargetClass = true, exposeProxy = true)
 public class BiBackedApplication {
 
     public static void main(String[] args) {
