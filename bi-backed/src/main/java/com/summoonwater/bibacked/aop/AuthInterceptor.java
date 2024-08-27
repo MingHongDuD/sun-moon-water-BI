0aa7a715502fb72aed8dcc12262727393f76a06a
SummoonWater <ming.hong.du.d@gmail.com>
2024-08-27 15:13:51 +0800

    用户登陆权限校验

--- bi-backed/src/main/java/com/summoonwater/bibacked/aop/AuthInterceptor.java
+++ bi-backed/src/main/java/com/summoonwater/bibacked/aop/AuthInterceptor.java
@@ -1,16 +1,22 @@
 package com.summoonwater.bibacked.aop;
 
-import cn.hutool.core.date.StopWatch;
 import com.summoonwater.bibacked.annotation.AuthCheck;
+import com.summoonwater.bibacked.common.ErrorCode;
+import com.summoonwater.bibacked.exception.BusinessException;
+import com.summoonwater.bibacked.model.entity.User;
+import com.summoonwater.bibacked.model.enums.UserRoleEnum;
+import com.summoonwater.bibacked.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
 /**
@@ -23,6 +29,9 @@ import javax.servlet.http.HttpServletRequest;
 @Slf4j
 public class AuthInterceptor {
 
+    @Resource
+    private UserService userService;
+
     /**
      * 执行拦截
      *
@@ -30,14 +39,33 @@ public class AuthInterceptor {
      * @param authCheck authCheck
      * @return result
      */
+    @Around("@annotation(authCheck)")
     public Object doInterceptor(ProceedingJoinPoint joinPoint, AuthCheck authCheck) throws Throwable {
         String mustRole = authCheck.mustRole();
         RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
-        HttpServletRequest request = ((ServletRequestAttributes) requestAttributes).getRequest();
-        // todo 当前登陆用户，登陆成功后返回用户
+        HttpServletRequest request = null;
+        if (requestAttributes != null) {
+            request = ((ServletRequestAttributes) requestAttributes).getRequest();
+        }
+        // 当前登录用户
+        User loginUser = userService.getLoginUser(request);
         // 必须有该权限才通过
         if (StringUtils.isNoneBlank(mustRole)) {
-
+            UserRoleEnum mustUserRoleEnum = UserRoleEnum.getEnumByValue(mustRole);
+            if (mustUserRoleEnum == null) {
+                throw new BusinessException(ErrorCode.NO_AUTH_ERROR);
+            }
+            // 如果被封号，直接拒绝
+            if (UserRoleEnum.BAN.equals(mustUserRoleEnum)) {
+                throw new BusinessException(ErrorCode.NO_AUTH_ERROR);
+            }
+            String userRole = loginUser.getUserRole();
+            // 必须有管理员权限
+            if (UserRoleEnum.ADMIN.equals(mustUserRoleEnum)) {
+                if (!mustRole.equals(userRole)) {
+                    throw new BusinessException(ErrorCode.NO_AUTH_ERROR);
+                }
+            }
         }
         // 通过权限校验，放行
         return joinPoint.proceed();
